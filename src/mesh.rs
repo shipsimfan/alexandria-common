f0aaf5bf42d5acf33ebd039732941a508e0f793a
Lance Hart <shipsimfan@hotmail.com>
2022-09-04 21:45:12 -0400

    Added update_vertices and update_indices

--- src/mesh.rs
+++ src/mesh.rs
@@ -9,6 +9,15 @@ pub trait Mesh<V>: Sized {
         window: &mut Self::Window<I>,
     ) -> Result<Self, Box<dyn std::error::Error>>;
 
+    fn update_vertices<I: Input>(
+        vertices: &[V],
+        window: &mut Self::Window<I>,
+    ) -> Result<(), Box<dyn std::error::Error>>;
+    fn update_indices<I: Input>(
+        indices: &[u32],
+        window: &mut Self::Window<I>,
+    ) -> Result<(), Box<dyn std::error::Error>>;
+
     fn render(&mut self);
 }
 
